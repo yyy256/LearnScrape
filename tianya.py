4d35c5c462211882d8866d6701ad221addd17476
yyy256 <yyy256@qq.com>
2014-10-18 19:34:44 +0800

    update

--- tianya.py
+++ tianya.py
@@ -1,20 +1,40 @@
+# coding:utf-8
+
+import sys
 import requests
 from bs4 import BeautifulSoup
+import re
 
+# 找出帖子版块、id、总页数、楼主
+url = sys.argv[1]
+
+r = requests.get(url)
+soup = BeautifulSoup(r.content)
+h = soup.find_all('div', {'class' : 'atl-menu clearfix js-bbs-act'})
+louzhu= h[0].get('js_activityusername')
+
+tiezi = soup.find_all("form", {"method" : "get", "action" : ""})
+res = re.findall(r'goPage\((.*?)\);', tiezi[0].get('onsubmit'))[0].split(',')
+bankuai = res[1].replace("'", "")
+id = res[2]
+page = res[3]
+
+# 抓出帖子内容
 f  = open('text.txt', 'w')
-for i in range(1, 484):
-	url = "http://bbs.tianya.cn/post-no05-146165-" + str(i) + ".shtml"
+for i in range(1, int(page)):
+	url = "http://bbs.tianya.cn/post-" + bankuai + "-" + id + "-" + str(i) + ".shtml"
 	r = requests.get(url)
 
 	soup = BeautifulSoup(r.content)
 
-	g_data = soup.find_all("div", {"js_username": "%E5%8E%86%E5%8F%B2%E6%8C%91%E5%B1%B1%E5%B7%A5"})
+	g_data = soup.find_all("div", {"_host": louzhu})
 	for item in g_data:
-		text = item.contents[3].find_all('div', {'class', 'bbs-content'})[0].text.strip()
-		if "=============" in text or '----------------' in text:
+		text = item.find_all("div", {"class" : re.compile("bbs-content")})[0].text.strip()
+		if len(text) < 1000:
 			pass
 		else:
-
 			f.write('%s\n'  %(text.encode('utf-8')))
 
 f.close()
+
+print "Done"
