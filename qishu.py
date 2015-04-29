18c0b45987011e1714b8caaa475ec75162cc9a6b
yyy256 <yyy256@qq.com>
2015-04-29 22:47:21 +0800

    update

--- qishu.py
+++ qishu.py
@@ -1,18 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'yaoyu'
+
 import requests
 from bs4 import BeautifulSoup
 import re
+import os
+
+if not os.path.exists('/home/yaoyu/金庸'):
+    os.mkdir('/home/yaoyu/金庸')
+os.chdir('/home/yaoyu/金庸')
 
 url = "http://www.qisuu.com/s/1/"
 
-get_url = requests.get(url)
-book_html = BeautifulSoup(get_url.content)
+get_urls = requests.get(url)
+books_html = BeautifulSoup(get_urls.content)
 
-all_books = book_html.find_all('div', {'class': 'listBox'})
+all_books = books_html.find_all('div', {'class': 'listBox'})
 
 for book in all_books[0].find_all(href=re.compile('Shtml')):
     book_url = 'http://www.qisuu.com' + book.get('href')
     get_book = requests.get(book_url)
+    book_html = BeautifulSoup(get_book.content)
+    dl_url = book_html.find_all(href=re.compile("http://dzs.qisuu.com/txt"))[0].get('href')
+    dl_book = requests.get(dl_url)
 
-    book_name = re.sub(r'[\r\n\t\《》]', '', book.text)
+    book_name = re.sub(u'[\u300a\u300b\u5168\u96c6]', '', book.text)
     with open(book_name + '.txt', 'wb') as f:
-        f.write()
+        f.write(dl_book.content)
